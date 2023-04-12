3f5a89d647f57033c1f5c35d743787e37ffaa821
James L <jameslloyd2007@hotmail.co.uk>
2023-04-12 16:49:56 +0100

    add def read_dna - works correctly

--- dna.py
+++ dna.py
@@ -1,1 +1,15 @@
 sample = ['GTA','GGG','CAC']
+
+def read_dna(dna_file):
+    dna_data = ""
+    with open(dna_file, "r") as f:
+        for line in f:
+            dna_data += line
+    return dna_data
+
+#print read_dna("suspect1.txt") testing reads correctly
+#print read_dna("suspect2.txt") testing reads correctly
+#print read_dna("suspect3.txt") testing reads correctly
+
+
+
